0a606709ff620e43696756a5086e3872f19cf38b
nightmare-tech <arpitguptaa004@gmail.com>
2021-09-21 01:01:53 +0530

    PWD_Vault update

--- PWD_Vault.py
+++ PWD_Vault.py
@@ -1,172 +1,183 @@
 import base64
-from endecrypt2 import decrypt
-from endecrypt2 import encrypt
+from getpass import getpass
+from endecrypt2 import *
 from endecrypt1 import gen_key
 from encoder import enco 
-from maincsv import entryc
-from maincsv import readc
-from maincsv import searchc
+from maincsv import *
+from banners import *
+import random
 
+def banner_p():
+    fn_l = [one, two, three, four, five, six, seven, eight, ninne, ten, eleven, twelve, thirteen, fourteen, fifteen,sixteen, seventeen, eighteen]
+    
+    random.choice(fn_l)()
 def login_screen():
+    try:
 
-    print('|*****************WELCOME*******************|')
-    print('|-------------------------------------------|')
-    print('|----------------PWD_Vault                  |')
-    print('|-------------------------------------------|')
-    print('|----------Developed by Arpit Gupta         |')
-    print('|-------------------------------------------|')
-    print('|--------------LOGIN SCREEN                 |')
-    print('|-------------------------------------------|')
-    print('|----1. Enter the Master Password           |')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|--s = First-Time-Setup (Only run this once)|')
-    print('|You may not be able to acess your files if |')
-    print('|if this is run again.                      |')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')    
-    print('|----h = Help                               |')
-    print('|----q = quit                               |')
-    print('|-------------------------------------------|')
-    print('|!!Do not use Ctrl + C to exit the program!!|')
-    print('|!!always use the \'q\' option to exit    !!|')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-
-
-
-    option = input('> ')
-    if option == '1':
-        with open('nothinghere.dat', 'rb') as bt_file:
-            decodedBytes = bt_file.read()
-            decodedStr = str(decodedBytes, 'utf-8')
-                        
-            decoded_pwd = base64.b64decode(decodedStr)
-            str_pwd = decoded_pwd.decode()
-            password = input('Enter Master Password:\n> ')
-
-        if str_pwd == password:
-            print('Login Success!')
-            decrypt()
-            main_menu()
-        else: 
-            print('Wrong Password')
-            input("Press any key to return to the login screen...\n> ")
-            login_screen()
-          
-    elif option == 's':
+        print('|-------------------------------------------------------|')
+        print('|--------------------LOGIN SCREEN ----------------------|')
+        print('|-------------------------------------------------------|')
+        print('|----1. Enter the Master Password ----------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|--s = First-Time-Setup (Only run this once) -----------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')    
+        print('|----h = Help ------------------------------------------|')
+        print('|----q = quit ------------------------------------------|')
+        print('|-------------------------------------------------------|')
         print('\n')
-        enco()
-        print('\n')
-        gen_key()
-        print('Great! Now remeber the master passwrd that you encoded as it will not be provided again in future...')
-        print("'Remember one Strong Password than a 100 weak ones...'")
-        input("Press any key to go to the main menu...\n> ")
-        main_menu()
 
-    elif option == 'h':
-        with open('help.txt', 'r') as manual:
-            reader = manual.read()
-            print(reader)
+
+
+        option = input('> ')
+        if option == '1':
+            with open('nothinghere.dat', 'rb') as bt_file:
+                decodedBytes = bt_file.read()
+                decodedStr = str(decodedBytes, 'utf-8')
+                            
+                decoded_pwd = base64.b64decode(decodedStr)
+                str_pwd = decoded_pwd.decode()
+                password = getpass("Enter Master Password:\n> ")
+
+            if str_pwd == password:
+                print('Login Success!')
+                decrypt()
+                main_menu()
+            else: 
+                print('Wrong Password!!')
+                login_screen()
+            
+        elif option == 's':
             print('\n')
-            input("Press any key to return to the login screen...\n> ")
+            enco()
+            print('\n')
+            gen_key()
+            print('Great! Now remeber the master passwrd that you encoded as it will not be provided again in future...')
+            print("'Remember one Strong Password than a 100 weak ones...'")
+            input("Hit Enter to go to the main menu...\n> ")
+            main_menu()
+
+        elif option == 'h':
+            with open('help.txt', 'r') as manual:
+                reader = manual.read()
+                print(reader)
+                print('\n')
+                input("Hit Enter to return to the login screen...\n> ")
+                login_screen()
+
+        elif option == 'q':
+                print("Exiting...")
+                quit()
+
+        elif option == 'banner':
+            banner_p()
             login_screen()
 
-    elif option == 'q':
-            print("Exiting...")
-            quit()
-    else:
-        print('Invalid Option')
-        input("Press any key to return to the login screen...\n> ")
+        else:
+            print('Invalid Option\n')
+            login_screen()
+    except KeyboardInterrupt:
+        print("\n")
         login_screen()
-
+        
 
 
 def main_menu():
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|-----------------PWD_Vault                 |')
-    print('|-------------------------------------------|')
-    print('|-----------------MAIN MENU                 |')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|-----1. Enter in Databse                   |')
-    print('|-----2. Read all the stored data           |')
-    print('|-----3. Search within all the stored data  |')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|h = Help                                   |')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
-    print('|e = erase the file                         |')
-    print('|q = quit                                   |')
-    print('|-------------------------------------------|')
-    print('|-------------------------------------------|')
+    try:
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-----------------------MAIN MENU ----------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-----1. Enter in Record -------------------------------|')
+        print('|-----2. Read all the stored data ----------------------|')
+        print('|-----3. Search within all the stored data -------------|')
+        print('|-----4. Delete a record -------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|h = Help ----------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|e = erase the file ------------------------------------|')
+        print('|q = quit ----------------------------------------------|')
+        print('|-------------------------------------------------------|')
+        print('|-------------------------------------------------------|')
 
-    option = input('> ')
-    if option == '1':
-        print('\n')
-        entryc()
-        print('\n')
-        input("Press any key to return to the main menu\n> ")
-        main_menu()
+        option = input('> ')
+        if option == '1':
+            print('\n')
+            entryc()
+            print('\n')
+            input("Hit Enter to return to the main menu\n> ")
+            main_menu()
 
-    elif option == '2':
-        print('\n')
-        readc()
-        print('\n')
-        input("Press any key to return to the main menu\n> ")
-        main_menu()
+        elif option == '2':
+            print('\n')
+            readc()
+            print('\n')
+            input("Hit Enter to return to the main menu\n> ")
+            main_menu()
 
-    elif option == '3':
-        print('\n')
-        searchc()
-        print('\n')
-        input("Press any key to return to the main menu\n> ")
-        main_menu()
+        elif option == '3':
+            print('\n')
+            searchc()
+            print('\n')
+            input("Hit Enter to return to the main menu\n> ")
+            main_menu()
 
-    elif option == 'h':
-        print('\n')
-        with open('help.txt', 'r') as manual:
-            reader = manual.read()
-            print(reader)
-            input("Press any key to return to the main menu\n> ")
-        print('\n')
-        main_menu()
+        elif option =='4':
+            print('\n')
+            deletec()
+            input("Hit Enter to return to the main menu\n> ")
+            main_menu()
 
-    elif option == 'e':
-        print('Are you sure you that you want to erase the data?(y/n) ')
-        sure = input('> ')
-        if sure == 'y':
-            with open('data.csv', 'w') as slaughter:
-                slaughter.truncate()
-                print('The data will never be found again!')
-                input("Press any key to return to the main menu\n> ")
+        elif option == 'h':
+            print('\n')
+            with open('help.txt', 'r') as manual:
+                reader = manual.read()
+                print(reader)
+                input("Hit Enter to return to the main menu\n> ")
+            print('\n')
+            main_menu()
+
+        elif option == 'e':
+            print('Are you sure you that you want to erase the data?(y/n) ')
+            sure = input('> ')
+            if sure == 'y':
+                with open('data.csv', 'w') as slaughter:
+                    slaughter.truncate()
+                    print('The data will never be found again!')
+                    input("Hit Enter to return to the main menu\n> ")
+                    main_menu()
+
+            elif sure == 'n':
+                print('No Problem')
                 main_menu()
-
-        elif sure == 'n':
-            print('No Problem')
+            else:
+                print('\nINVALID OPTION')
+                input("Hit Enter to return to the main menu\n> ")
+                main_menu()
+        
+        elif option == 'q':
+            print('Encrypting Data...')
+            encrypt()
+            print('Exiting...')
+            quit()
+        
+        elif option == 'banner':
+            banner_p()
             main_menu()
         else:
-            print('\nINVALID OPTION')
-            input("Press any key to return to the main menu\n> ")
+            print('INVALID OPTION!')
             main_menu()
-    
-    elif option == 'q':
-        print('Encrypting Data...')
-        encrypt()
-        print('Exiting...')
-        quit()
 
-    else:
-        print('INVALID OPTION!')
+    except KeyboardInterrupt:
+        print("\n!!!!!Use 'q' to exit!!!!")
         main_menu()
-
-
-
-
+   
+banner_p()
 login_screen()
-
 
--- maincsv.py
+++ maincsv.py
@@ -1,20 +1,18 @@
 import csv
+from os import read
 import pandas
 
 
 def entryc():
     with open('data.csv', 'a') as cfile:
-        fields = ['Website/Service', 'Name of User', 'Username/Email-Id', 'Password']
-        entry = csv.DictWriter(cfile, fieldnames= fields)
-
-        while True:
-            service = input("Enter the name of website/ service provider:\n> ")
-            name = input("Enter your name:\n> ")
-            user = input("Enter the username/ email-id by which the password is stored:\n> ")                
-            pwd_1 = input("Enter the password:\n Double check the password then hit Return\n> ")
-            pwd = input("Enter the Password again...\n>")
+       entry = csv.DictWriter(cfile, fieldnames=['Website/Service', 'Username/Email-Id', 'Password'])
+       while True:
+            service = input("Enter the name of ````website/ service provider```:\n> ")
+            user = input("Enter the ```username/ email-id``` by which the password is stored:\n> ")                
+            pwd_1 = input("Enter the ```Password```:\n Double check the password then hit Return\n> ")
+            pwd = input("Enter the ```Password``` again...\n> ")
             if pwd_1 == pwd:
-                l_dict = {'Website/Service': service, 'Name of User': name, 'Username/Email-Id': user, 'Password': pwd}
+                l_dict = {'Website/Service': service, 'Username/Email-Id': user, 'Password': pwd}
                 entry.writerow(l_dict)
                 print('Recorded Successfully')
 
@@ -33,47 +31,49 @@ def entryc():
                 print('INVALID OPTION!')
                 break
 def readc():
-    # table = Table(title='Passwords')
-
-    # table.add_column('Website/Serivce', justify='right', style='green', no_wrap=True)
-    # table.add_column('Username/Email-Id', style='cyan')
-    # table.add_column('Detail Category')
-    # table.add_column('Password', style='red')
 
     print('_'*80)
-    csv_data = pandas.read_csv('data.csv', names=['Website/Service', 'Name of User', 'Username/Email-Id', 'Password'])
+    csv_data = pandas.read_csv('data.csv', names=['Website/Service', 'Username/Email-Id', 'Password'])
     print(csv_data)
     print('_'*80)
 
 
 
 def searchc():
-    count = 1
-    items = []
+    
+    query = input("Find(Not case sensitive):\n> ")
     with open('data.csv', 'r') as cfile:
         reader = csv.reader(cfile)
-        for rows in reader:
-            items.append(rows)
+        for row in reader:
+            for col in row:
+                if col.lower() in query.lower():
+                    print('|-------------------------------------------------------|')
+                    print('Website/Service   :',row[0])
+                    print('Username/Email-Id :',row[1])
+                    print('Password          :',row[2])        
+                    print('|-------------------------------------------------------|')
         
-    query = input("Find(Not case sensitive):\n> ")
+        
+        
+def deletec():
+    try: 
+        df = pandas.read_csv('data.csv', names=['Website/Service', 'Username/Email-Id', 'Password'])
+        print(df)
 
-    for item in items:
+        index_number = input("Enter the Index Number of Row to be deleted:\n> ")
 
-        if query.lower() in str(item).lower():
-            print('_'*20)
-            print(count, '.')
-            print('Website/Serice:', item[0])
-            print('Name of User:', item[1])
-            print('Username/Email-Id:', item[2])
-            print('Password:', item[3])
-            print('_'*20)
-            count += 1
+        df = df.drop([int(index_number)])
+        df.to_csv('data.csv', index=False, header=False)
 
-    print(':'* 80)   
-    
-
-
+        print("\nEdited Record: ")
+        print(df)
+    except ValueError:
+        print('Not a number try again!')
+        print("Nothing was deleted...")
+        
+        
 if __name__ == '__main__': 
     entryc()
     readc()
     searchc()
+    deletec()
